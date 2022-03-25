bc8011767f357fa41039b22d3162cf9aff6ef67e
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2022-03-25 16:27:04 -0600

    Update script_bd.sql

--- script_bd.sql
+++ script_bd.sql
@@ -14,3 +14,19 @@ CREATE TABLE ginecologas (
 //USUARIO DE PRUEBA
 INSERT INTO ginecologas (nombres,apellidoPaterno, apellidoMaterno,cedulaProfesional,cedulaEspecialista,telefono,correo,clave) 
 VALUES ('Esbeydi','Villa', 'Juan','1234567890','1234567890','1234567890','esbeydi@correo.com','ZXNiZXlkaTEyMw=='); 
+//Tabla "CITAS"
+CREATE TABLE citas (
+  idcitas SERIAL NOT NULL,
+  nombre varchar(45) NOT NULL,
+  hora varchar(5) NOT NULL,
+  dia varchar(2) NOT NULL,
+  mes varchar(2) NOT NULL,
+  descripcion text NOT NULL,
+  idginecologa int NOT NULL,
+  estado integer NOT NULL,
+  PRIMARY KEY (idcitas)
+);
+//FK de "GINECOLOGAS" a "CITAS"
+ALTER TABLE citas
+ADD FOREIGN KEY (idginecologa) REFERENCES ginecologas(idginecologa);
+
